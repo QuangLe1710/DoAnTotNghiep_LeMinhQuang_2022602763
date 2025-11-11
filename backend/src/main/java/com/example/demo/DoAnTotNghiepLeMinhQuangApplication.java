54c9a487aa5a30637b857ff688cbbbbb4cfa51b9
QuangLe1710 <leminhquang.td2004@gmail.com>
2025-11-11 22:53:14 +0700

    init project

---
+++ backend/src/main/java/com/example/demo/DoAnTotNghiepLeMinhQuangApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnTotNghiepLeMinhQuangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnTotNghiepLeMinhQuangApplication.class, args);
+	}
+
+}
